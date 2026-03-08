31baf44206b79275655c5074aa2b7887adfce18b
unknown <priyanshupandey9931@gmail.com>
2026-03-08 21:51:12 +0530

    UC2_Assignment_Print_Banner

--- App/src/OOPSBannerApp.java
+++ App/src/OOPSBannerApp.java
@@ -1,6 +1,9 @@
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to OOPS Banner App");
-        System.out.println("OOPS");
+package com.oopsbanner;
+
+public class Banner {
+    public void printBanner() {
+        System.out.println("*****************************");
+        System.out.println("        OOPS BANNER APP      ");
+        System.out.println("*****************************");
     }
 }
